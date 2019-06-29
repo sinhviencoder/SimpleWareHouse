8cd72f2be5bb8b8279f3ecd6204febec0bc20a57
sinhviencoder <48917649+sinhviencoder@users.noreply.github.com>
2019-06-29 11:44:09 +0700

    Final commit

--- src/com/company/LoadingFromStagingToWareHouse.java
+++ src/com/company/LoadingFromStagingToWareHouse.java
@@ -12,6 +12,7 @@ import com.company.db.MySqlDBConnection;
 
 public class LoadingFromStagingToWareHouse {
 	static Calendar vn = Calendar.getInstance(TimeZone.getTimeZone("Asia/Bangkok"));
+
 	public static void main(String[] args) throws SQLException {
 		DBConnectionUtil cnUtil = new MySqlDBConnection(MySqlDBConnection.STAGING_URL, MySqlDBConnection.USER_NAME,
 				MySqlDBConnection.PASSWORD);
@@ -21,13 +22,13 @@ public class LoadingFromStagingToWareHouse {
 				+ " WHERE st.file_status='extracting_suc'";
 		ResultSet ready_staging_information = cnn.createStatement()
 				.executeQuery(queryStagingReadyForLoadingToWareHouse);
-		String from_staging_table = "", warehouse_require_cols = "", warehouse_cols = "",to_warehouse_table = "";
+		String from_staging_table = "", warehouse_require_cols = "", warehouse_cols = "", to_warehouse_table = "";
 		while (ready_staging_information.next()) {
 			from_staging_table = ready_staging_information.getString("staging_table");
 			warehouse_require_cols = ready_staging_information.getString("warehouse_require_cols");
 			warehouse_cols = ready_staging_information.getString("warehouse_cols");
 			to_warehouse_table = ready_staging_information.getString("warehouse_table");
-			
+
 			String queryGetDataFromCurrentStaging = "".concat("SELECT ").concat(warehouse_require_cols)
 					.concat(" FROM STAGING.").concat(from_staging_table);
 			ResultSet dataRequiredForWareHouse = cnn.createStatement().executeQuery(queryGetDataFromCurrentStaging);
@@ -39,19 +40,19 @@ public class LoadingFromStagingToWareHouse {
 				if (rsOfIfExist.next()) {
 					// update existed record to inactive
 					String queryUpdateOldRecordToInActive = "UPDATE WAREHOUSE." + to_warehouse_table
-							+ " SET is_active=0,changed_time='" +(new Timestamp(System.currentTimeMillis()).toString()).substring(0,19)+"'" 
+							+ " SET is_active=0,changed_time='"
+							+ (new Timestamp(System.currentTimeMillis()).toString()).substring(0, 19) + "'"
 							+ " WHERE natural_key=" + natural_key;
 					cnn.createStatement().executeUpdate(queryUpdateOldRecordToInActive);
 				}
 				// insert new record
-				String insertNewRecord = "INSERT INTO WAREHOUSE." + to_warehouse_table 
-										+ "(" + warehouse_cols + ")" + 
-										" SELECT " + warehouse_require_cols + " FROM STAGING." + from_staging_table
-										 + " WHERE NATURAL_KEY=" + natural_key;
+				String insertNewRecord = "INSERT INTO WAREHOUSE." + to_warehouse_table + "(" + warehouse_cols + ")"
+						+ " SELECT " + warehouse_require_cols + " FROM STAGING." + from_staging_table
+						+ " WHERE NATURAL_KEY=" + natural_key;
 				System.out.println(insertNewRecord);
 				try {
-				cnn.createStatement().executeUpdate(insertNewRecord);
-				}catch (Exception e) {
+					cnn.createStatement().executeUpdate(insertNewRecord);
+				} catch (Exception e) {
 					System.out.println(e.getMessage());
 				}
 			}
